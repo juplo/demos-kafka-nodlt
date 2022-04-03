a4ff2a4b45f4927231f06f67077b55cb43b3254a
Kai Moritz <kai@juplo.de>
2022-04-03 09:26:56 +0200

    Setup aus der Intro-Übung übernommen

--- README.sh
+++ README.sh
@@ -6,9 +6,27 @@ then
   exit
 fi
 
-trap 'kill $(jobs -p) 2>/dev/null' EXIT
-
 docker-compose up -d
 
 echo "Waiting for the Kafka-Cluster to become ready..."
 docker-compose exec cli cub kafka-ready -b kafka:9092 1 60 > /dev/null 2>&1 || exit 1
+
+docker-compose ps
+
+echo
+echo "Hilfe-Ausgabe von kafkacat"
+echo
+docker-compose exec -T cli kafkacat -h
+echo
+echo "Nachrichten schreiben mit kafkacat"
+echo
+docker-compose exec -T cli kafkacat -P -b kafka:9092 -t test << EOF
+Hallo Welt!
+Nachricht #1
+Nachricht #2
+Nachricht #3
+EOF
+echo
+echo "Nachrichten lesen mit kafkacat"
+echo
+docker-compose exec cli kafkacat -C -b kafka:9092 -t test -o beginning -e
