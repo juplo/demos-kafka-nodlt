059ff32e02e2b882388f3e4d4d332c60aba0a4b4
Kai Moritz <kai@juplo.de>
2023-06-11 14:46:07 +0200

    Docker-Setup in das Unterverzeichnis `docker` verschoben

--- README.sh
+++ README.sh
@@ -2,27 +2,27 @@
 
 if [ "$1" = "cleanup" ]
 then
-  docker-compose down -t0 -v --remove-orphans
+  docker-compose -f docker/docker-compose.yml down -t0 -v --remove-orphans
   exit
 fi
 
-docker-compose up -d kafka-1 kafka-2 kafka-3
-docker-compose ps
+docker-compose -f docker/docker-compose.yml up -d kafka-1 kafka-2 kafka-3
+docker-compose -f docker/docker-compose.yml ps
 echo "Waiting for the Kafka-Cluster to become ready..."
-docker-compose run --rm cli cub kafka-ready -b kafka:9092 3 60 > /dev/null 2>&1 || exit 1
+docker-compose -f docker/docker-compose.yml run --rm cli cub kafka-ready -b kafka:9092 3 60 > /dev/null 2>&1 || exit 1
 
-docker-compose up -t0 -d cli
+docker-compose -f docker/docker-compose.yml up -t0 -d cli
 sleep 1
-docker-compose logs setup
+docker-compose -f docker/docker-compose.yml logs setup
 
 echo
 echo "Hilfe-Ausgabe von kafkacat"
 echo
-docker-compose exec -T cli kafkacat -h
+docker-compose -f docker/docker-compose.yml exec -T cli kafkacat -h
 echo
 echo "Nachrichten schreiben mit kafkacat"
 echo
-docker-compose exec -T cli kafkacat -P -b kafka:9092 -t test << EOF
+docker-compose -f docker/docker-compose.yml exec -T cli kafkacat -P -b kafka:9092 -t test << EOF
 Hallo Welt!
 Nachricht #1
 Nachricht #2
@@ -31,4 +31,4 @@ EOF
 echo
 echo "Nachrichten lesen mit kafkacat"
 echo
-docker-compose exec cli kafkacat -C -b kafka:9092 -t test -o beginning -e
+docker-compose -f docker/docker-compose.yml exec cli kafkacat -C -b kafka:9092 -t test -o beginning -e
