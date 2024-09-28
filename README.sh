b8d8c6bee4065fa398124fcf082adf4b752a6f81
Kai Moritz <kai@juplo.de>
2024-09-28 07:43:48 +0200

    Schreibweise des CLI-Kommandos für Docker-Compose korrigiert

--- README.sh
+++ README.sh
@@ -2,25 +2,25 @@
 
 if [ "$1" = "cleanup" ]
 then
-  docker-compose -f docker/docker-compose.yml down -t0 -v --remove-orphans
+  docker compose -f docker/docker-compose.yml down -t0 -v --remove-orphans
   exit
 fi
 
-docker-compose -f docker/docker-compose.yml up --remove-orphans setup || exit 1
-docker-compose -f docker/docker-compose.yml ps
+docker compose -f docker/docker-compose.yml up --remove-orphans setup || exit 1
+docker compose -f docker/docker-compose.yml ps
 
-docker-compose -f docker/docker-compose.yml up -t0 -d cli
+docker compose -f docker/docker-compose.yml up -t0 -d cli
 sleep 1
-docker-compose -f docker/docker-compose.yml logs setup
+docker compose -f docker/docker-compose.yml logs setup
 
 echo
 echo "Hilfe-Ausgabe von kafkacat"
 echo
-docker-compose -f docker/docker-compose.yml exec -T cli kafkacat -h
+docker compose -f docker/docker-compose.yml exec -T cli kafkacat -h
 echo
 echo "Nachrichten schreiben mit kafkacat"
 echo
-docker-compose -f docker/docker-compose.yml exec -T cli kafkacat -P -b kafka:9092 -t test << EOF
+docker compose -f docker/docker-compose.yml exec -T cli kafkacat -P -b kafka:9092 -t test << EOF
 Hallo Welt!
 Nachricht #1
 Nachricht #2
@@ -29,4 +29,4 @@ EOF
 echo
 echo "Nachrichten lesen mit kafkacat"
 echo
-docker-compose -f docker/docker-compose.yml exec cli kafkacat -C -b kafka:9092 -t test -o beginning -e
+docker compose -f docker/docker-compose.yml exec cli kafkacat -C -b kafka:9092 -t test -o beginning -e
