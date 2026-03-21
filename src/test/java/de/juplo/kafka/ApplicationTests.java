7c13023eaef1122013a30ba078b66bf444656655
Kai Moritz <kai@juplo.de>
2026-03-21 13:38:38 +0100

    `ApplicationTests` verwendet die echte MVC-Implementierung
    * Der Test verwendet jetzt nicht mehr `@AutoconfigureMockMvc`.
    * Statdessen wird `RANDOM_PORT` und das `TestRestTemplate` verwendet.
    * Außerdem:
    ** Logging für den Zookeeper- und den Broker-Prozess unterdrückt.
    ** `ApplicationTests` überarbeitet/aufgeräumt.

--- src/test/java/de/juplo/kafka/ApplicationTests.java
+++ src/test/java/de/juplo/kafka/ApplicationTests.java
@@ -1,48 +1,44 @@
 package de.juplo.kafka;
 
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.Duration;
-
-import static de.juplo.kafka.ApplicationTests.PARTITIONS;
+import static de.juplo.kafka.ApplicationTests.NUM_PARTITIONS;
 import static de.juplo.kafka.ApplicationTests.TOPIC;
-import static org.awaitility.Awaitility.await;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
   properties = {
     "juplo.bootstrap-server=${spring.embedded.kafka.brokers}",
-    "juplo.consumer.topic=" + TOPIC })
-@AutoConfigureMockMvc
-@EmbeddedKafka(topics = TOPIC, partitions = PARTITIONS)
+    "juplo.consumer.topic=" + TOPIC,
+    "logging.level.de.juplo.kafka=TRACE",
+  })
 @DirtiesContext
+@EmbeddedKafka(topics = TOPIC, partitions = NUM_PARTITIONS)
 public class ApplicationTests
 {
-  static final String TOPIC = "FOO";
-  static final int PARTITIONS = 10;
-
-  @Autowired
-  MockMvc mockMvc;
-
-
-
   @Test
   public void testApplicationStartup()
   {
-    await("Application is healthy")
-      .atMost(Duration.ofSeconds(5))
-      .untilAsserted(() -> mockMvc
-        .perform(get("/actuator/health"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("status").value("UP")));
+    ResponseEntity<String> response = restTemplate.getForEntity("/actuator/health", String.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(HttpStatus.OK.value()));
+    assertThat(JsonPath.parse(response.getBody()).read("$.status", String.class)).isEqualTo("UP");
   }
+
+
+  static final String TOPIC = "ExampleConsumerTest_TEST";
+  static final int NUM_PARTITIONS = 7;
+
+  @Autowired
+  TestRestTemplate restTemplate;
 }
